bf0a576e207ad1924b23edde0540b35300fa4fd3
Mary <maeliz_84@hotmail.com>
2019-03-28 23:42:23 -0500

    funciones de la calculadora completas

--- js/app.js
+++ js/app.js
@@ -17,41 +17,42 @@ var Calculadora = {
       //this.teclas[i].onkeyup   = this.eventRestaurarTeclaPequeña;
     }
   },
-
+  // funcion que atieende al evento onclick de las teclas
   eventTeclaOnClick: function(event) {
     var display = document.getElementById('display');
+    var valorPantalla = display.textContent;
 
     switch (event.target.id) {
       case "on":
-        display.textContent = 0;
-        display.ultimoValor = null;
+        valorPantalla = limpiar(display);
         break;
       case "sign":
-        addSign(display);
-        break;
-      case "dividido":
-        colocarOperacion(display, "%");
-        break;
-      case "por":
-        colocarOperacion(display, "*");
-        break;
-      case "menos":
-        colocarOperacion(display, "-");
+        valorPantalla = colocarSigno(display);
         break;
       case "punto":
-         addPoint(display);
+        valorPantalla = colocarPunto(display);
         break;
       case "igual":
-        realizaOperacion(display);
+        valorPantalla = realizaOperacion(display);
+        break;
+      case "dividido":
+        valorPantalla = colocarOperacion(display, "%");
+        break;
+      case "por":
+        valorPantalla = colocarOperacion(display, "*");
+        break;
+      case "menos":
+        valorPantalla = colocarOperacion(display, "-");
         break;
       case "mas":
-        colocarOperacion(display, "+");
+        valorPantalla = colocarOperacion(display, "+");
         break;
       default:
-        addNumbers(display, event.target.id);
+        valorPantalla = colocarNumero(display, event.target.id);
         break;
     }
-    console.log('click', event.target);
+    // coloca en la pantalla el valor actual
+    display.textContent = valorPantalla;
   },
 
   eventTeclaPequenia: function(event){
@@ -60,7 +61,7 @@ var Calculadora = {
     console.log('d', event.target);
   },
 
-  eventRestaurarTeclaPequeña: function(event){
+  eventRestaurarTeclaPequenia: function(event){
     event.target.style.width = "22%";
     event.target.style.height = "62.91px";
     console.log('f', event.target);
@@ -68,58 +69,128 @@ var Calculadora = {
 
 }
 
+// limpiar
+function limpiar(){
+    display.ultimoNumero  = null;
+    display.primerNumero  = null;
+    display.operacion     = null;
+    display.hizoOperacion = false;
+    return 0;
+}
+
 // coloca el signo
-function addSign(display){
-  var curValue = display.textContent*1;
-  if(curValue != 0){
-      curValue = curValue*-1;
+function colocarSigno(display){
+  var valor = display.textContent*1;
+  if(display.hizoOperacion) {
+    limpiar();
   }
-  display.textContent = curValue;
+  if(valor != 0){
+    valor = valor*-1;
+  }
+  return valor;
 }
 
 // coloca los numeros en la pantalla
-function addNumbers(display, number){
-  var curValue = display.textContent;
-  if(curValue == "0"){
-    if(number == "0"){ return }
-    curValue = "";
+function colocarNumero(display, numero){
+  var valor = display.textContent;
+  // si realizo operacion coloca el cero mas el numero ingresado
+  if(display.hizoOperacion){
+    limpiar();
+    valor = "0";
   }
-  if(curValue.length < 8){
-      curValue += number;
-  }number == "0"
-  display.textContent = curValue;
+  if(valor == "0"){
+    if(numero == "0"){ return }
+    valor = "";
+  }
+  if(valor.length < 8){
+      valor += numero;
+  }
+  return valor;
 }
 
 // coloca los numeros en la pantalla
-function addPoint(display){
-  var curValue = display.textContent;
-  if(curValue.indexOf(".") < 0){
-      curValue += ".";
+function colocarPunto(display){
+  var valor = display.textContent;
+  if(display.hizoOperacion) return limpiar();
+  if(valor.indexOf(".") < 0){
+      valor += ".";
   }
-  display.textContent = curValue;
+  return valor;
 }
 
 // colocar operacion y ultimo valor
 function colocarOperacion(display, operacion){
-  display.ultimoValor = display.textContent;
-  display.operacion   = operacion;
-  display.textContent = "";
+  if(display.operacion && !display.hizoOperacion){ // si ya tiene definida operacion solo caia la operacion
+    display.operacion     = operacion;
+  }else{
+    display.primerNumero  = display.textContent;
+    display.operacion     = operacion;
+    display.hizoOperacion = false;
+  }
+  return "";
 }
 
 // colocar operacion y ultimo valor
 function realizaOperacion(display){
-  if(!display.ultimoValor){
-    return;
+  var resultado;
+
+  if(!display.primerNumero || display.textContent == ""){
+    return display.textContent;
   }
+
+  var primerNumero      = (display.primerNumero*1);
+  var segundoNumero     = display.hizoOperacion ? display.ultimoNumero*1 : (display.textContent*1);
+  display.ultimoNumero  = display.hizoOperacion ? display.ultimoNumero : display.textContent;
+  display.hizoOperacion = true;
 
   switch (display.operacion) {
     case "+":
-      display.textContent = (display.textContent*1) + (display.ultimoValor*1);
-      display.ultimoValor = null;
+      resultado = garantizaMaximoCaracteres(primerNumero + segundoNumero);
       break;
-    default:
-    break;
+    case "-":
+      resultado = garantizaMaximoCaracteres(primerNumero - segundoNumero);
+      break;
+    case "*":
+      resultado = garantizaMaximoCaracteres(primerNumero * segundoNumero);
+      break;
+    case "%":
+      if(segundoNumero == 0) {
+         limpiar();
+         display.hizoOperacion = true;
+         return "ERROR";
+      }
+      resultado = garantizaMaximoCaracteres(primerNumero / segundoNumero);
+      break;
   }
+
+  display.primerNumero = resultado;
+  resultado = resultado + "";
+  // el resultado debe ser menor que ocho caracteres
+  if(resultado.length > 8){
+      limpiar();
+      display.hizoOperacion = true;
+      return "INFINITY";
+  }
+  return resultado;
+}
+
+// garantiza el maximo de caracteres truncando los decimales
+function garantizaMaximoCaracteres(numero){
+  var str = numero + "";
+  if(str.length > 8){
+      var enteros   = parseInt(numero);
+      var lenEntero = (enteros + "").length;
+      if(lenEntero <= 8){
+        return toFixed(numero, 8 - (lenEntero+1));
+      }
+  }
+  return numero;
+}
+
+function toFixed(num, fixed) {
+    fixed = fixed || 0;
+    fixed = Math.pow(10, fixed);
+    return Math.floor(num * fixed) / fixed;
 }
 
 Calculadora.init();
